aa15c2f915b761db5580e776a7701c38b83f5965
Cuno de Boer <cuno.de.boer@gmail.com>
2022-08-04 23:21:41 +0200

    Implemented showPetById

--- src/main/kotlin/nl/codestar/springpetclinickotlin/api/PetsApiDelegateImpl.kt
+++ src/main/kotlin/nl/codestar/springpetclinickotlin/api/PetsApiDelegateImpl.kt
@@ -1,19 +1,30 @@
 package nl.codestar.springpetclinickotlin.api
 
 import nl.codestar.springpetclinickotlin.model.PetDto
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import org.springframework.web.server.ResponseStatusException
+
+val database = listOf(
+	PetDto(0L, "Charlie"),
+	PetDto(1L, "Obi"),
+	PetDto(2L, "Loki"),
+	PetDto(3L, "Cuno"),
+)
 
 @Component
 class PetsApiDelegateImpl : PetsApiDelegate {
 	override fun listPets(limit: Int?): ResponseEntity<List<PetDto>> = ResponseEntity(
-		listOf(
-			PetDto(1L, "Charlie"),
-			PetDto(2L, "Obi"),
-			PetDto(3L, "Loki"),
-			PetDto(3L, "Cuno"),
-		), HttpStatus.OK
+		database, OK
 	)
 
+	override fun showPetById(petId: String): ResponseEntity<PetDto> =
+		database.find { it.id == petId.toLong() }?.let {
+			ResponseEntity(
+				it, OK
+			)
+		} ?: throw ResponseStatusException(
+			NOT_FOUND, "entity not found"
+		)
 }
